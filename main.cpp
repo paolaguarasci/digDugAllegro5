0254e56e322442b4a4718c151223c451b4a871b0
Paola Guarasci <paolaguarasci@gmail.com>
2019-05-26 15:56:24 +0200

    esc with any key down

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int main(int argc, char **argv) {
   ALLEGRO_DISPLAY *display = NULL;
+  ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 
   if (!al_init()) {
     cerr << "failed to initialize allegro!\n";
@@ -15,9 +16,24 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  al_clear_to_color(al_map_rgb(0, 0, 0));
-  al_flip_display();
-  al_rest(10.0);
+  al_install_keyboard();
+
+  event_queue = al_create_event_queue();
+  al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+  while (true) {
+    ALLEGRO_EVENT ev;
+    al_wait_for_event(event_queue, &ev);
+    if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+      break;
+    }
+
+    // Schermo nero
+    al_clear_to_color(al_map_rgb(0, 0, 0));
+    al_flip_display();
+  }
+
+  al_uninstall_keyboard();
   al_destroy_display(display);
 
   return 0;
